4e207e83fc62fa97f278321173222d03e839d3fc
dutran90 <ngocdu12@gmail.com>
2015-01-09 14:33:23 +0700

    Create introduction a swift tour project
    Upload 3 files of project Introduction a swift tour by Binladenit

---
+++ IntroductionASwiftTour.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+// This is the first demo swift code by Binladenit
+
+import UIKit
+
+var str = "Hello, playground"
+
+var str1 = "Hello, i'm Binladenit"
+
+println(str1)
+
+println("\(str1) and \(str)")
+
+// Have fun ^ ^
